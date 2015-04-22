b115c998ef985b7d655dbd2d9bafca913cf75611
liujin <kanshouji911@163.com>
2015-04-22 22:52:00 +0800

    test

--- js/main.js
+++ js/main.js
@@ -196,14 +196,16 @@ document.addEventListener('DOMContentLoaded',function(){
 								//添加APP js文件
 								var oScr = document.createElement('script');
 								oScr.src = 'js/'+target.id+'.js';
-								oHead.appendChild(oScr);
+								oAppBox.addEventListener('load',function(){
+									oHead.appendChild(oScr);
+									//关闭事件
+									addClose(oScr,oLink);
+								},false);
 								//添加APP css文件
 								var oLink = document.createElement('link');
 								oLink.rel = 'stylesheet';
 								oLink.href = 'css/'+target.id+'.css'
 								oHead.appendChild(oLink);
-								//关闭事件
-								addClose(oScr,oLink);
 							},500);
 						});
 					},100);
