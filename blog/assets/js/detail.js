6ae9b426868a0c64ecd2b11e8fa61741d7bd827f
liujin <liujin@autohome.com.cn>
2016-07-15 09:30:48 +0800

    add detail.html

--- blog/assets/js/detail.js
+++ blog/assets/js/detail.js
@@ -1,7 +1,7 @@
 var detail = getQueryString('detail');
 $.ajax({
   type:'GET',
-  url:'../assets/data/' + detail + '.data',
+  url:'assets/data/' + detail + '.data',
   dataType:'text', 
   success:function(data) {
     $('.detail').html(data);
