a0c6656a2fab5bfb8b4808ea33f2eeafb1722340
liujin <liujin@autohome.com.cn>
2016-07-15 09:20:24 +0800

    add detail.html

---
+++ blog/assets/js/detail.js
@@ -0,0 +1,38 @@
+var detail = getQueryString('detail');
+$.ajax({
+  type:'GET',
+  url:'assets/data/' + detail + '.data',
+  dataType:'text', 
+  success:function(data) {
+    $('.detail').html(data);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getQueryString(name) {                  // 获取url参数
+  'use strict';
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+  var r = decodeURIComponent(location.search).substring(1).match(reg);
+  if (r != null) {
+    return r[2];
+  }
+  return null;
+}
--- blog/assets/js/main.js
+++ blog/assets/js/main.js
@@ -6,11 +6,11 @@ $.ajax({
     var html = [];
     for (var i = 0; i < data.list.length; i++) {
       var value = data.list[i];
-      html.push('<div class="article-list">\
+      html.push('<a href="./detail.html?detail=' + value.detail + '" class="article-list">\
         <h3 class="article-title"><i class="type-icon">' + value.type + '</i>' + value.title + '</h3>\
         <p class="article-des">' + value.des + '</p>\
         <p><time>' + value.time + '</time></p>\
-      </div>');
+      </a>');
     }
     $('.home-bd').append(html.join(''));
   }
